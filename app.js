7137726c51008250cc2d06d84508fb33df4b76f1
Monika Śleboda <monikakozak60@gmail.com>
2023-07-02 08:38:30 +0200

    Make POST request

--- app.js
+++ app.js
@@ -7,46 +7,28 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function (req, res) {
 
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+   
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Suturday";
-            break;
-        default:
-        console.log("Error: current day is equal to: " + currentDay);
-    }
+    var day = today.toLocaleDateString("en-US", options);
 
-
-    res.render("list", {
-        kindOfDay: day 
-    });
+    res.render("list", {kindOfDay: day});
 
 });
 
-
+app.post ("/", function(req,res){
+    var item = req.body.newItem;
+    console.log(item);
+});
 
 app.listen(3000, function () {
     console.log("Server started on port 3000.");
