e8b141272c4cf5d9ca8520e875a8312e7c3b7322
Monika Śleboda <monikakozak60@gmail.com>
2023-07-04 21:32:13 +0200

    Add CSS

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ let items = ["Buy Food", "Cook Food", "Eat food"];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
 
