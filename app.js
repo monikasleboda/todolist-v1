3112ae367bcf9ab3f129d5d79e16da3fb1cc09fc
Monika Śleboda <monikakozak60@gmail.com>
2023-07-04 11:40:39 +0200

    Change var to let

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = ["Buy Food", "Cook Food", "Eat food"];
+let items = ["Buy Food", "Cook Food", "Eat food"];
 
 app.set('view engine', 'ejs');
 
@@ -13,22 +13,22 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function (req, res) {
 
-    var today = new Date();
+    let today = new Date();
    
-    var options = {
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
 
-    var day = today.toLocaleDateString("en-US", options);
+    let day = today.toLocaleDateString("en-US", options);
 
     res.render("list", {kindOfDay: day, newListItems: items});
 
 });
 
 app.post ("/", function(req,res){
-    var item = req.body.newItem;
+    let item = req.body.newItem;
 
     items.push(item);
 
