929da5cafc1ffd1faa1eb7c40de6ebd9668f2cb9
Monika Śleboda <monikakozak60@gmail.com>
2023-07-05 11:36:44 +0200

    Create Work List using layout

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = ["Buy Food", "Cook Food", "Eat food"];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -24,17 +25,34 @@ app.get("/", function (req, res) {
 
     let day = today.toLocaleDateString("en-US", options);
 
-    res.render("list", {kindOfDay: day, newListItems: items});
-
+    res.render("list", {listTitle: day, newListItems: items});
 });
 
 app.post ("/", function(req,res){
+
     let item = req.body.newItem;
 
-    items.push(item);
+    if (req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
 
-    res.redirect("/");
 });
+
+
+app.get("/work", function(req,res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.post("/work", function(req,res){
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+})
+
 
 app.listen(3000, function () {
     console.log("Server started on port 3000.");
