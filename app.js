7ebef8240de0703de6c482c01e860099fc24678e
Monika Śleboda <monikakozak60@gmail.com>
2023-07-05 13:53:05 +0200

    Refactor the code

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat food"];
-let workItems = [];
+const items = ["Buy Food", "Cook Food", "Eat food"];
+const workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -14,23 +15,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-
-    let today = new Date();
-   
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    let day = today.toLocaleDateString("en-US", options);
-
+    const day = date.getDate();
     res.render("list", {listTitle: day, newListItems: items});
 });
 
 app.post ("/", function(req,res){
 
-    let item = req.body.newItem;
+    const item = req.body.newItem;
 
     if (req.body.list === "Work"){
         workItems.push(item);
